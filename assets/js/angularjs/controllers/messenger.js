60b6a03e2e667110d97f6620d6e4e9b033ec6e1a
Alberto Souza <alberto.souza.99@gmail.com>
2013-09-25 04:56:30 -0300

    messenger angular controller, css and contact-list template added

---
+++ assets/js/angularjs/controllers/messenger.js
@@ -0,0 +1,53 @@
+"use strict";
+
+angular.module("application").controller("MessengerCtrl", [
+  "$rootScope", "$scope", "$location", "SessionService", function($rootScope, $scope, $location, SessionService) {
+    var init;
+
+    init = function() {
+      $scope.templates = [ { name: 'contact-list.html', url: 'templates/.ejs'} ];
+      $scope.template = $scope.templates[0];
+      $scope.contacts = [
+        {
+          id: 1,
+          name: 'Maria'
+        },
+        {
+          id: 2,
+          name: 'Joana'
+        }
+      ];
+
+      $scope.inComunication = [
+        {
+          id: 1,
+          name: 'Maria',
+          messages: [{
+            id : 1,
+            content : 'oi mundo'
+          }]
+        },
+        {
+          id: 2,
+          name: 'Santos',
+          messages: [
+          {
+            id : 10,
+            content : 'Oi,'
+          },
+          {
+            id : 11,
+            content : 'Tudo bem?'
+          },
+          ],
+        }
+      ];
+
+      return $scope.user = {};
+    };
+
+
+    console.log($scope);
+    return init();
+  }
+]);
---
+++ assets/linker/js/messenger.js
@@ -0,0 +1,13 @@
+(function() {
+  jQuery(function() {
+    jQuery(".messenger-off").click(function() {
+      jQuery(".messenger-off").hide();
+      return jQuery(".messenger").show();
+    });
+    return jQuery(".messenger .header").click(function() {
+      jQuery(".messenger").hide();
+      return jQuery(".messenger-off").show();
+    });
+  });
+
+}).call(this);
