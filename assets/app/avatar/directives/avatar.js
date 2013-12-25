da3cf5b924dd8de30fcb719bffd0bcc263265c41
Alberto Souza <alberto.souza.99@gmail.com>
2013-12-25 01:18:25 -0200

    suport for links added in avatar directive

--- assets/app/avatar/directives/avatar.js
+++ assets/app/avatar/directives/avatar.js
@@ -12,28 +12,36 @@
       '$compile',
       function($compile) {
         return{
-          restrict:"EAC",
+          restrict:"A",
           link:function (scope, elm, attrs) {
-            console.log('attrs',attrs);
-
-            var size_class;
-
-            switch(attrs['avatarSize']) {
-              case 'medium':
-                size_class = ' width="150px" height="150px" class="img-rounded avatar-medium" ';
-                break;
-              default:
-                size_class = ' width="50px" height="50px" class="img-rounded avatar-small" ';
-            }
 
             scope.$watch(attrs.avatar, function (uid) {
               var tag;
+              var avatarLink = '';
+              var size_class;
+
+              if(attrs['avatarLink']){
+                avatarLink = attrs['avatarLink'];
+              }
+
+              switch(attrs['avatarSize']) {
+                case 'medium':
+                  size_class = ' width="150px" height="150px" class="img-rounded avatar-medium" ';
+                  break;
+                default:
+                  size_class = ' width="50px" height="50px" class="img-rounded avatar-small" ';
+              }
 
               if ((uid !== null) && (uid !== undefined) && (uid !== '')) {
                 tag = '<img '+size_class+' data-ng-src="/user/avatar/' + uid +'">';
               } else {
                 tag = '<img '+size_class+' data-ng-src="/imgs/avatars/user-avatar.png">';
               }
+
+              if(avatarLink){
+                tag = '<a href="'+avatarLink+'">'+ tag +'</a>';
+              }
+
               elm.html( $compile(tag)(scope) );
             });
           }
